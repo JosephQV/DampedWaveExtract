34a0224952fed1944d96e397413497f32d500743
JosephQV <112574271+JosephQV@users.noreply.github.com>
2024-07-09 23:00:31 -0400

    Included RMS Measured, STDs, and plot of Accuracy by Noise

--- MCMC.py
+++ MCMC.py
@@ -1,11 +1,12 @@
 import numpy as np
-import functools
+from wave_func import mcmc_wave
 
 class MCMCModel:
     def __init__(self, function, param_ranges: np.ndarray, function_kwargs: dict = None):
         self.function = function
         self.param_ranges = param_ranges
         self.function_kwargs = function_kwargs
+        self.rng = np.random.default_rng(seed=824)
     
     def generate_noise(self, data, noise_amp=1.0):
         """
@@ -18,15 +19,19 @@ class MCMCModel:
         Returns:
         - numpy array of noisy data
         """
-        noise = 2 * noise_amp * np.random.random(size=len(data)) - noise_amp
+        noise = 2 * noise_amp * self.rng.random(size=len(data)) - noise_amp
         return data + noise
 
     def compute_rms(self, observed, predicted):
         diff = observed - predicted
         return np.sqrt(np.mean(diff**2))
 
-    def guess_params(self):
-        return np.random.uniform(low=self.param_ranges[:,0], high=self.param_ranges[:,1], size=len(self.param_ranges))
+    def guess_params(self, distribution=None):
+        if distribution == "norm":
+            mu = (self.param_ranges[:,0] + self.param_ranges[:,1]) / 2
+            sigma = mu/10
+            return self.rng.normal(mu, sigma, size=len(self.param_ranges))
+        return self.rng.uniform(low=self.param_ranges[:,0], high=self.param_ranges[:,1], size=len(self.param_ranges))
         
     def likelihood(self, noisy_vals, params, tol=1e-9):
         trial_vals = eval(self.function.__name__)(*params, **self.function_kwargs)
@@ -41,7 +46,7 @@ class MCMCModel:
 
         current_likelihood = self.likelihood(noisy_vals, current_params)
 
-        samples = np.zeros(shape=(num_iterations, len(self.param_ranges)))
+        samples = np.empty(shape=(num_iterations, len(self.param_ranges)))
         samples[0] = current_params
         
         for i in range(1, num_iterations):
@@ -57,119 +62,37 @@ class MCMCModel:
 
         return samples
     
-    def thin_samples(self, samples, thin_ratio: float = 0.2, cutoff_start: int = 0):
-        cutoff = samples[cutoff_start:]
-        thinned = np.zeros_like(cutoff)
-        i = 0
-        mod = int(1/thin_ratio)
-        for j in range(cutoff.size):
-            if j % mod == 0:
-                thinned[i] = cutoff[j]
-                i += 1
+    def thin_samples(self, samples, thin_percentage: float = 0.25, cutoff_percentage: float = 0.2):
+        if thin_percentage == 0:
+            return np.array([])
+        mod = int(1 / thin_percentage)
+        start = int(samples.size * cutoff_percentage)
+        thinned = np.zeros_like(samples)
+        j = 0
+        for i in range(start, samples.size):
+            if i % mod == 0:
+                thinned[j] = samples[i]
+                j += 1
         return np.trim_zeros(thinned)
     
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    from wave_func import mcmc_wave
-    REAL_AMPLITUDE = 6.0
-    REAL_DAMPING = 0.3
-    REAL_OMEGA = 2.0
-    REAL_PHASE = 0.0
-    NOISE_AMPLITUDE = 4.0
-    NUM_ITERATIONS = 100000
-    DATA_STEPS = 10000
-    THIN_RATIO = 0.1
-    
-    time, real_wave = mcmc_wave(REAL_AMPLITUDE, REAL_DAMPING, REAL_OMEGA, steps=DATA_STEPS, return_time=True)
-    
-    ranges = np.array(
-        [
-            [0.1, 10.0],    # amplitude (A) range
-            [0.1, 3.0],     # damping (b) range
-            [0.1, 10.0],    # angular frequency (omega) range
-            # [0.0, 2*np.pi]  # phase constant range
-        ]
-    )
-    kwargs = {"seconds": 30.0, "steps": DATA_STEPS, "return_time": False}
-
-    model = MCMCModel(function=mcmc_wave, param_ranges=ranges, function_kwargs=kwargs)
-    samples = model.metropolis_hastings(real_wave, NUM_ITERATIONS, NOISE_AMPLITUDE)
-    noise = model.generate_noise(real_wave, noise_amp=NOISE_AMPLITUDE)
-
-    for i, sample in enumerate(samples):
-        current_params = sample
-        acceptance_status = "Accepted"
-        
-        if i > 0:
-            previous_params = samples[i - 1]
+    def print_sample_statuses(self, samples):
+        for i, sample in enumerate(samples):
+            current_params = sample
+            acceptance_status = "Accepted"
             
-            if np.all(current_params == previous_params):
-                acceptance_status = "Rejected (same as previous)"
+            if i > 0:
+                previous_params = samples[i - 1]
+                
+                if np.all(current_params == previous_params):
+                    acceptance_status = "Rejected (same as previous)"
 
-        print(f"Trial {i+1}: ({acceptance_status})\n{current_params}")
+            print(f"Trial {i+1}: ({acceptance_status})\n{current_params}")
     
-    amplitude_samples = samples[:, 0]
-    damping_samples = samples[:, 1]
-    angular_freq_samples = samples[:, 2]
-    # phase_samples = samples[:, 3]
-    
-    amplitude_samples = model.thin_samples(amplitude_samples, thin_ratio=THIN_RATIO, cutoff_start=int(NUM_ITERATIONS/10))
-    damping_samples = model.thin_samples(damping_samples, thin_ratio=THIN_RATIO, cutoff_start=int(NUM_ITERATIONS/10))
-    angular_freq_samples = model.thin_samples(angular_freq_samples, thin_ratio=THIN_RATIO, cutoff_start=int(NUM_ITERATIONS/10))
-    
-    mean_amplitude = np.mean(amplitude_samples)
-    mean_damping = np.mean(damping_samples)
-    mean_omega = np.mean(angular_freq_samples)
-    
-    result_wave = mcmc_wave(mean_amplitude, mean_damping, mean_omega, steps=DATA_STEPS)
 
-    figure = plt.figure(figsize=(13, 7), layout="constrained")
-    figure.suptitle("Extracting Signal Parameters From Noise")
-    axes = figure.subplot_mosaic(
-    """
-    WWN.
-    ABF.
-    """,
-    width_ratios=[1, 1, 1, 0.05]
-    )
+if __name__ == "__main__":
+    model = MCMCModel(function=mcmc_wave, param_ranges=np.arange(3), function_kwargs={"steps":5, "seconds": 12})
+    print(model.likelihood(np.arange(5), [400, 400, 400]))
     
-    axes["A"].hist(amplitude_samples, bins=100, density=True, label="estimated distribution")
-    axes["A"].set_xlabel("Amplitude")
-    ylim = axes["A"].get_ylim()
-    axes["A"].plot([REAL_AMPLITUDE, REAL_AMPLITUDE], [0.0, ylim[1]], label="true value")
-    axes["A"].plot([mean_amplitude, mean_amplitude], [0.0, ylim[1]], label="sample mean")
-    axes["A"].legend(prop={"size": 8})
-    
-    axes["B"].hist(damping_samples, bins=100, density=True, label="estimated distribution")
-    axes["B"].set_xlabel("Damping")
-    ylim = axes["B"].get_ylim()
-    axes["B"].plot([REAL_DAMPING, REAL_DAMPING], [0.0, ylim[1]], label = "true value")
-    axes["B"].plot([mean_damping, mean_damping], [0.0, ylim[1]], label="sample mean")
-    axes["B"].legend(prop={"size": 8})
-    
-    axes["F"].hist(angular_freq_samples, bins=100, density=True, label="estimated distribution")
-    axes["F"].set_xlabel("Angular frequency")
-    ylim = axes["F"].get_ylim()
-    axes["F"].plot([REAL_OMEGA, REAL_OMEGA], [0.0, ylim[1]], label = "true value")
-    axes["F"].plot([mean_omega, mean_omega], [0.0, ylim[1]], label="sample mean")
-    axes["F"].legend(prop={"size": 8})
-    
-    # axes["P"].hist(phase_samples, bins=100, density=True, label="estimated distribution")
-    # axes["P"].set_xlabel("phase constant")
-    # axes["P"].plot([REAL_PHASE, REAL_PHASE], [0.0, 1.0], label = "true value")
-    # axes["P"].legend()
-        
-    axes["W"].plot(time, real_wave, "r", label="real")
-    axes["W"].set_xlabel("time")
-    axes["W"].annotate(f"Real Parameters:\nAmplitude: {REAL_AMPLITUDE}\nDamping: {REAL_DAMPING}\nAngular Frequency: {REAL_OMEGA}", xy=(0.75, 0.1), xycoords="axes fraction")
-    axes["W"].plot(time, result_wave, "g", label="generated")
-    axes["W"].legend()
-    
-    axes["N"].plot(time, noise)
-    axes["N"].set_xlabel("time")
-    axes["N"].annotate(f"Noise amplitude: {NOISE_AMPLITUDE}", xy=(0.3, 0.9), xycoords="axes fraction")
-
-    plt.show()
     
     
     
---
+++ accuracy.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from MCMC import MCMCModel
+from wave_func import mcmc_wave
+
+def run_trial(model, real_amp, real_damp, real_angf, steps, timespan, thin_percentage, cutoff, num_iterations, noise_amplitude):
+    # use given params to make a real wave
+    real_wave = mcmc_wave(real_amp, real_damp, real_angf, seconds=timespan, steps=steps)
+    # run metropolis hastings for the given number of iterations and given noise
+    samples = model.metropolis_hastings(data=real_wave, num_iterations=num_iterations, noise_amplitude=noise_amplitude)
+    # get the mean of the samples returned for each parameter, create the wave generated from them
+    mean_amplitude = np.mean(model.thin_samples(samples[:,0], thin_percentage, cutoff))
+    mean_damping = np.mean(model.thin_samples(samples[:,1], thin_percentage, cutoff))
+    mean_angular_freq = np.mean(model.thin_samples(samples[:,2], thin_percentage, cutoff))
+    generated_wave = mcmc_wave(mean_amplitude, mean_damping, mean_angular_freq, seconds=timespan, steps=steps)
+    # find the rms between the real and generated waves
+    rms = model.compute_rms(observed=real_wave, predicted=generated_wave)
+    return rms
+
+REAL_AMPLITUDE = 7.0
+REAL_DAMPING = 0.3
+REAL_ANGULAR_FREQ = 1.2
+NOISE_AMPLITUDE = np.linspace(0.0, 7.5, 75)
+NUM_ITERATIONS = 10000
+DATA_STEPS = 10000
+TIMESPAN = 20
+THIN_PERCENTAGE = 0.5 # between 0 and 0.5. (0.5 would use half the returned samples, 0 would use none of them, above 0.5 would use all of them)
+CUTOFF_START = 0.5
+
+ranges = np.array(
+    [
+        [0.1, 10.0],    # amplitude (A) range
+        [0.1, 3.0],     # damping (b) range
+        [0.1, 10.0],    # angular frequency (omega) range
+    ]
+)
+kwargs = {"seconds": TIMESPAN, "steps": DATA_STEPS, "return_time": False}
+
+model = MCMCModel(mcmc_wave, param_ranges=ranges, function_kwargs=kwargs)
+
+accuracy_by_noise = []
+for noise in NOISE_AMPLITUDE:
+    rms = run_trial(model, REAL_AMPLITUDE, REAL_DAMPING, REAL_ANGULAR_FREQ, DATA_STEPS, TIMESPAN, THIN_PERCENTAGE, CUTOFF_START, NUM_ITERATIONS, noise)
+    accuracy_by_noise.append(rms)
+figure = plt.figure()
+axes = figure.add_subplot()
+axes.scatter(NOISE_AMPLITUDE, accuracy_by_noise)
+axes.set_xlabel("Noise Amplitude")
+axes.set_ylabel("Accuracy (RMS)")
+figure.suptitle("Wave Estimation Accuracy as a Function of Noise")
+
+plt.show()
